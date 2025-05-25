669ef70a156e35352a31444cf748ef2532f2ded3
zapfruit <kamakhya.bh@gmail.com>
2025-05-26 04:08:33 +0530

    Add new folder: build

---
+++ build/precache-manifest.61ad6ba8622020f7cb2b274c4b6076c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df27d329592835719a0db4e10cd19fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2e2378e15eba2cfe0b2",
+    "url": "/static/css/main.a34417a4.chunk.css"
+  },
+  {
+    "revision": "ad7b6f9758c3823ead26",
+    "url": "/static/js/2.f68473b3.chunk.js"
+  },
+  {
+    "revision": "8a721b77919ac5b264ba0684d0da5769",
+    "url": "/static/js/2.f68473b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7629225f86b370c60f",
+    "url": "/static/js/3.855ac7f4.chunk.js"
+  },
+  {
+    "revision": "e2e2378e15eba2cfe0b2",
+    "url": "/static/js/main.db596175.chunk.js"
+  },
+  {
+    "revision": "fa6bbf38c60d216f0b88",
+    "url": "/static/js/runtime-main.9ee204f0.js"
+  }
+]);
